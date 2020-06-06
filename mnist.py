8ca234828ecda08f48d11d569de19fc2ad20f45c
shishupal-cmd <64471690+shishupal-cmd@users.noreply.github.com>
2020-06-06 12:53:10 +0530

    change

--- mnist.py
+++ mnist.py
@@ -12,8 +12,19 @@ Y_test_cat=to_categorical(Y_test)
 from keras.models import Sequential 
 from keras.layers import Dense 
 model = Sequential()
+import tweak
+import maths
 model.add(Dense(units=512 ,input_dim=28*28 ,activation='relu'))
 model.add(Dense(units=256 , activation='relu'))
 model.add(Dense(units=10 , activation = 'softmax'))
 from keras.optimizers import RMSprop
 model.compile(optimizer =RMSprop() , loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit(X_train_1d,Y_train_cat,epochs=tweak.epochs_no)
+accuracy=model.evaluate(X_test_1d,Y_test_cat)[1]
+accuracy=round(accuracy*100,3)
+print(f"accuracy {accuracy} %")
+from os import system
+if accuracy > 95:
+   system("echo 'true' > ./accuracy.txt)
+else:
+   system("echo 'false' > ./accuracy.txt)
